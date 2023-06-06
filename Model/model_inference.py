85f1208b0ecede6ade785d8ec13e4b2b1d1737b2
yunama47 <yunama1236@gmail.com>
2023-06-06 11:03:01 +0800

    update inference script

--- Model/model_inference.py
+++ Model/model_inference.py
@@ -5,11 +5,11 @@ try:
 except ImportError:
     print('dependency not installed')
 
-def _sigmoid_prediction(outImageModel, outTabularModel):
+def _sigmoid_prediction(linear_predictions):
     sigmoid = Dense(1, activation='sigmoid', 
                     kernel_initializer='ones', 
                     bias_initializer='zeros')
-    X = np.array([[outImageModel[0][0],outTabularModel[0][0]]])
+    X = np.array([[x[0][0] for x in linear_predictions]])
     return sigmoid(X)
 
 def inference(model_paths:list=None,
@@ -18,21 +18,21 @@ def inference(model_paths:list=None,
     Parameters :
         model_path : list dari path kedua model (gambar dan tabel)
         data_points: list dari data yang akan diprediksi untuk masing masing model'''
-    models = []
-    for path in model_paths:
+    assert len(model_paths) == len(data_points), 'jumlah models dan data_poin tidak sesuai'
+    predictions = []
+    for i, path in enumerate(model_paths):
         try:
             model = tf.keras.models.load_model(path)
-            models.append(model)
-        except:
-            print(f'model not found in {path}')
-    predictions = []
-    for data, model in zip(data_points, models):
-        pred = model.predict(data)
-        predictions.append(pred)
+            pred = model.predict(data_points[i])
+            predictions.append(pred)
+        except Exception as e:
+            print('ERROR |',e)
     
-    return _sigmoid_prediction(predictions[0],predictions[1])
+    return _sigmoid_prediction(predictions) >= 0.5
     
 if __name__=='__main__':
-    prediction = inference(['../test.h5','../test.h5'],
-                           np.array([[[1,2,3]],[[4,5,6]]]))
-    print(prediction)
+    prediction = inference(['./csv_model.h5'],
+                           [np.array([[0,100,2.5,0,1]]),
+                            # np.array([[1,80,2.5,0,1]]),
+                            ])
+    print('yes' if prediction else 'no')
